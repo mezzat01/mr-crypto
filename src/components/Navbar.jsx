1002230a12afefb08a32f98d679c932c1a5c5f2a
mezzat01 <mezzatamine2002@gmail.com>
2022-03-23 23:25:26 +0100

    components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+/// Used rafce to create structure
+// Used antdesign for icons and UI
+
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+           <Avatar />
+           <Typography.Title level={2} className="logo">
+                 <Link to="/">MR Crypto</Link>
+           </Typography.Title>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
